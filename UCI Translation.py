1fa88fa5a86ebcdae9309fbc8cdb5693af4707b8
John3Yonce <129086801+John3Yonce@users.noreply.github.com>
2023-04-03 10:51:01 -0400

    Add files via upload
    Added stockfish functionality. CHANGE PATH TO CURRENT STOCKFISH EXECUTABLE PATH

--- UCI Translation.py
+++ UCI Translation.py
@@ -1,3 +1,8 @@
+from stockfish import Stockfish
+stockfish = Stockfish(path=r"C:\Users\yonce\Downloads\stockfish_15.1_win_x64_avx2(1)\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2")
+stockfish.set_elo_rating(850)
+stockfish.set_depth(15)
+print(stockfish.get_board_visual())
 moved_from = [-1, -1]
 from_coords = ['z', 'z']
 moved_to = [-1, -1]
@@ -13,20 +18,20 @@ temp_board = [['-', '-', '-', '-', '-', '-', '-', '-'],
               ['-', '-', '-', '-', '-', '-', '-', '-'],
               ['-', '-', '-', '-', '-', '-', '-', '-'],]
 prev_board = [['br1', 'bn1', 'bb1', 'bq', 'bk', 'bb2', 'bn2', 'br2'], 
-              ['bp1', 'bp2', '-', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'], 
+              ['bp1', 'bp2', 'bp3', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
-              ['-', '-', 'bp3', '-', '-', '-', '-', '-'], 
+              ['-', '-', '-', '-', '-', '-', '-', '-'], 
               ['wp1', 'wp2', 'wp3', 'wp4', 'wp5', 'wp6', 'wp7', 'wp8'],
               ['wr1', 'wn1', 'wb1', 'wq', 'wk', 'wb2', 'wn2', 'wr2']]
 curr_board = [['br1', 'bn1', 'bb1', 'bq', 'bk', 'bb2', 'bn2', 'br2'], 
-              ['bp1', 'bp2', '-', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'], 
+              ['bp1', 'bp2', 'bp3', 'bp4', 'bp5', 'bp6', 'bp7', 'bp8'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
+              ['-', '-', '-', 'wp4', '-', '-', '-', '-'], 
               ['-', '-', '-', '-', '-', '-', '-', '-'], 
-              ['-', '-', 'wp2', '-', '-', '-', '-', '-'], 
-              ['wp1', '-', 'wp3', 'wp4', 'wp5', 'wp6', 'wp7', 'wp8'],
+              ['wp1', 'wp2', 'wp3', '-', 'wp5', 'wp6', 'wp7', 'wp8'],
               ['wr1', 'wn1', 'wb1', 'wq', 'wk', 'wb2', 'wn2', 'wr2']]
 #while(True)
              
@@ -59,17 +64,26 @@ while i < 8:
         j+=1
     i+=1
 
-if cap:
-    print('{0} from'.format(piece), end = ' ')
-    print(*from_coords, sep='', end = ' ')
-    print('captured {0} at'.format(piece2), end = ' ')
-    print(*to_coords, sep='')
-else:
-    print('Moved {0} from'.format(piece), end = ' ')
-    print(*from_coords, sep='', end = ' ')
-    print('to', end = ' ')
-    print(*to_coords, sep='')
+
 
 #For Stockfish
 move_input = from_coords[0] + str(from_coords[1]) + to_coords[0] + str(to_coords[1])
 #reset all vars down here and update board states
+if stockfish.is_move_correct(str(move_input)):
+    stockfish.make_moves_from_current_position([move_input])
+    if cap:
+        print('{0} from'.format(piece), end = ' ')
+        print(*from_coords, sep='', end = ' ')
+        print('captured {0} at'.format(piece2), end = ' ')
+        print(*to_coords, sep='')
+    else:
+        print('Moved {0} from'.format(piece), end = ' ')
+        print(*from_coords, sep='', end = ' ')
+        print('to', end = ' ')
+        print(*to_coords, sep='')
+else:
+    print('Move is illegal. Please try again.')
+print(stockfish.get_board_visual())
+stockfish.make_moves_from_current_position([stockfish.get_best_move_time(2000)])
+print(stockfish.get_board_visual())
+
